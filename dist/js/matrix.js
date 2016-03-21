4545dc04ba63b84130b07a91e5fee4ca92034c6e
Dale Hurwitz <daz20589@hotmail.com>
2016-03-21 17:34:34 +1100

    experimentation

--- dist/js/matrix.js
+++ dist/js/matrix.js
@@ -1,5 +1,61 @@
 "use strict";
 
-(function () {
-	console.log("matrix init");
-})();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Matrix = function () {
+	var Matrix = function () {
+		function Matrix(canvasId) {
+			_classCallCheck(this, Matrix);
+
+			this.BG_COLOUR = "#000";
+
+			this.canvas = document.getElementById(canvasId);
+			this.ctx = this.canvas.getContext("2d");
+			this.setup();
+			this.draw();
+		}
+
+		_createClass(Matrix, [{
+			key: "setup",
+			value: function setup() {
+				this.canvas.width = this.CANVAS_WIDTH = window.innerWidth;
+				this.canvas.height = this.CANVAS_HEIGHT = window.innerHeight;
+				this.canvas.style.display = "block";
+				this.canvas.style.backgroundColor = this.BG_COLOUR;
+				this.loop();
+			}
+		}, {
+			key: "draw",
+			value: function draw() {}
+		}, {
+			key: "update",
+			value: function update() {}
+		}, {
+			key: "loop",
+			value: function loop() {
+				this.update();
+				this.draw();
+				window.requestAnimationFrame(this.loop.bind(this));
+			}
+		}, {
+			key: "drawRect",
+			value: function drawRect(x, y, w, h, fill) {
+				this.setFill(fill);
+				this.ctx.fillRect(x, y, w, h);
+			}
+		}, {
+			key: "setFill",
+			value: function setFill(style) {
+				this.ctx.fillStyle = style;
+			}
+		}]);
+
+		return Matrix;
+	}();
+
+	return Matrix;
+}();
+
+var newMatrix = new Matrix("matrix-canvas");
--- src/js/matrix.js
+++ src/js/matrix.js
@@ -1,3 +1,48 @@
-(() => {
-	console.log("matrix init");
+const Matrix = (() => {
+	class Matrix {
+		constructor(canvasId) {
+			
+			this.BG_COLOUR = "#000";
+			
+			this.canvas = document.getElementById(canvasId);
+			this.ctx = this.canvas.getContext("2d");
+			this.setup();
+			this.draw();
+		}
+		
+		setup() {
+			this.canvas.width = this.CANVAS_WIDTH = window.innerWidth;
+			this.canvas.height = this.CANVAS_HEIGHT = window.innerHeight;
+			this.canvas.style.display = "block";
+			this.canvas.style.backgroundColor = this.BG_COLOUR;
+			this.loop();
+		}
+		
+		draw() {
+			
+		}
+		
+		update() {
+			
+		}
+		
+		loop() {
+			this.update();
+			this.draw();
+			window.requestAnimationFrame(this.loop.bind(this));
+		}
+		
+		drawRect(x, y, w, h, fill) {
+			this.setFill(fill);
+			this.ctx.fillRect(x, y, w, h);
+		}
+		
+		setFill(style) {
+			this.ctx.fillStyle = style;
+		}
+	}
+	
+	return Matrix;
 })();
+
+var newMatrix = new Matrix("matrix-canvas");
