b066e1880273debd5eccb37c780703bbf6063cc8
Dale Hurwitz <daz20589@hotmail.com>
2016-03-21 16:21:08 +1100

    init skeleton

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require("gulp");
+var babel = require("gulp-babel");
+var browserSync = require("browser-sync").create();
+
+var config = {
+	port: 3000,
+	paths: {
+		dist: "./dist",
+		mainJs: "./src/js/matrix.js"
+	}
+}
+
+gulp.task("js", function() {
+	return gulp.src(config.paths.mainJs)
+		.pipe(babel({
+			presets: ["es2015"]
+		}))
+		.pipe(gulp.dest(config.paths.dist + "/js"))
+		.pipe(browserSync.stream());
+})
+
+gulp.task("browserSync", function() {
+	browserSync.init({
+		port: config.port,
+		server: {
+			baseDir: "./"
+		}
+	});
+});
+	
+gulp.task("default", ["browserSync"], function() {
+	gulp.watch(config.paths.mainJs, ["js"]);
+})
